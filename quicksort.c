d546b037373d6f81fefc66c59b294055cf67498e
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-04-04 18:27:52 +1030

    Re-added quicksort

--- quicksort.c
+++ quicksort.c
@@ -6,7 +6,7 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:10:03 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/04 12:16:37 by njackson         ###   ########.fr       */
+/*   Updated: 2024/04/04 10:34:59 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,28 @@
 
 int	partition(int *arr, int start, int end)
 {
+	int	mid;
+	int	i;
 
+	mid = start;
+	while (arr[mid] < arr[end] && mid < end)
+	{
+		mid++;
+	}
+	i = mid + 1;
+	while (i < end)
+	{
+		if (arr[i] < arr[end])
+		{
+			ft_swap(arr + i, arr + mid++);
+		}
+		i++;
+	}
+	ft_swap(arr + end, arr + mid);
+	return (mid);
 }
 
-// NEEDS CHANGING
-t_list	*quicksort(int *arr, t_list *s_a, t_list *s_b)
+int	*quicksort(int *arr, int start, int end)
 {
 	int	mid;
 
