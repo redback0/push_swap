2f4fd09289f797efdc2ef188cc34eb72e0e2f606
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-04-04 11:13:11 +1030

    Added funcs to push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,19 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 10:26:16 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/04 10:27:23 by njackson         ###   ########.fr       */
+/*   Updated: 2024/04/04 11:06:37 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "libft.h"
+
+t_list	*get_instrs(int *arr);
+t_list	*run_instr(t_list *lst, char *instr);
+
+// quicksort functions
 int	partition(int *arr, int start, int end);
 int	*quicksort(int *arr, int start, int end);
 
