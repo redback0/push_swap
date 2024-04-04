5fa959f6c7ab8a2e9b9dae5d985869aa637c219e
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-04-04 18:26:15 +1030

    Added all push_swap instructions

--- push_swap.c
+++ push_swap.c
@@ -6,27 +6,73 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:07:17 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/04 12:20:05 by njackson         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:17:45 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_list	*rotate_stack(t_list **stack)
+void	rotate_stack(t_list **stack)
 {
+	t_list	head;
+	t_list	end;
+
+	end = *stack;
+	*stack = (*stack)->next;
+	head = ft_lstend(*stack);
+	head->next = end;
+	end->next = NULL;
 }
 
-t_list	*rev_rotate_stack(t_list **stack)
-
-t_list	*push_stack(t_list **to, t_list **from)
+void	rev_rotate_stack(t_list **stack)
 {
+	t_list	head;
+	t_list	end;
+
+	end = *stack;
+	while (end->next->next)
+		end = end->next;
+	head = end->next;
+	head->next = *stack;
+	end->next = NULL;
+	*stack = head;
 }
 
-t_list	*swap_stack(t_list **stack)
+void	push_stack(t_list **to, t_list **from)
 {
+	t_list	mv;
+	t_list	tmp;
+
+	if ((*from)->next)
+	{
+		tmp = *from;
+		while (tmp->next->next)
+			tmp = tmp->next;
+		mv = tmp->next;
+		tmp->next = NULL;
+	}
+	else
+	{
+		mv = *from;
+		*from = NULL;
+	}
+	if (*to)
+		ft_lstlast(*to)->next = mv;
+	else
+		*to = mv;
 }
 
-t_list	*do_instr(char *instr, t_list **s_a, t_list **s_b)
+void	swap_stack(t_list **stack)
+{
+	t_lst	tmp;
+
+	tmp = (*stack)->next
+	(*stack)->next = tmp->next;
+	tmp->next = *stack;
+	*stack = tmp;
+}
+
+void	do_instr(char *instr, t_list **s_a, t_list **s_b)
 {
 	if (!ft_strncmp("sa", instr, 3))
 		swap_stack(*s_a);
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,22 +6,20 @@
 /*   By: njackson <njackson@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:10:42 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/04 12:19:58 by njackson         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:41:14 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_list	swap_ab(t_list **s_a, t_list **s_b)
+void	swap_ab(t_list **s_a, t_list **s_b)
 {
 	swap_stack(s_a);
 	swap_stack(s_b);
-	return (ft_lstnew("ss"));
 }
 
-t_list	rotate_ab(t_list **s_a, t_list **s_b)
+void	rotate_ab(t_list **s_a, t_list **s_b)
 {
 	rotate_stack(s_a);
 	rotate_stack(s_b);
-	return (ft_lstnew("rr"));
 }
