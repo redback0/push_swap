a38e44c94ca65a9e1c559c894190cb10a457ab32
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-04-16 11:23:07 +0930

    Made test script better :)

--- test.sh
+++ test.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-INSTRS=$(./push_swap "$@")
+if [ $# -eq 1 ]; then
+#	STACK=($(shuf -i $(($1 * -10))-$(($1 * 10)) -n $1))
+	STACK=$(awk -v loop=$1 -v range=$(($1 * 20)) -v take=$(($1 * 10)) 'BEGIN{
+			srand()
+			do {
+				numb = 1 + int(rand() * range - take)
+				if (!(numb in prev)) {
+				print numb
+				prev[numb] = 1
+				count++
+				}
+			} while (count<loop)
+			}')
+	echo "--STACK--"
+	echo "$STACK"
+	echo "--END--"
 
-echo "$INSTRS" | wc -l
-echo "$INSTRS" | ./checker_linux "$@"
+	INSTRS=$(./push_swap ${STACK[@]})
+
+	echo "$INSTRS" | wc -l | tr -d ' '
+	echo "$INSTRS" | ./checker_Mac ${STACK[@]}
+fi
