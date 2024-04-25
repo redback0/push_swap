2778c425eb795ae0c39e12e6ad10a3aaf925b7fd
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-04-25 17:20:13 +0930

    Fixed norm

--- initial_sort.c
+++ initial_sort.c
@@ -36,40 +36,46 @@ static int	get_init_range(t_list *s_a)
 	return ((out >> 2) + 1);
 }
 
+static int	*get_first_last(t_list *s_a, int size, int *out)
+{
+	t_stack	*cont;
+	int		range;
+	int		i;
+
+	range = get_init_range(s_a);
+	out[0] = -1;
+	out[1] = -1;
+	i = 0;
+	while (s_a)
+	{
+		cont = s_a->content;
+		if (size - cont->fi < range)
+		{
+			if (out[0] < 0)
+				out[0] = i;
+			out[1] = i;
+		}
+		s_a = s_a->next;
+		i++;
+	}
+	return (out);
+}
+
 // I hate this function :/
 static void	init_rotator(t_list **s_a, t_list **s_b,
 	t_list **instr_lst, char **instrs)
 {
-	t_list	*tmp;
-	t_stack	*cont;
 	int		size;
-	int		first;
-	int		last;
-	int		range;
-	int		i;
+	int		*fl_no;
 
-	tmp = *s_a;
-	size = ft_lstsize(tmp);
-	range = get_init_range(*s_a);
-	first = -1;
-	last = -1;
-	i = 0;
-	while (tmp)
-	{
-		cont = tmp->content;
-		if (size - cont->fi < range)
-		{
-			if (first < 0)
-				first = i;
-			last = i;
-		}
-		tmp = tmp->next;
-		i++;
-	}
-	if (size - last < first)
+	size = ft_lstsize(*s_a);
+	fl_no = (int *)malloc(2 * sizeof(int));
+	get_first_last(*s_a, size, fl_no);
+	if (size - fl_no[1] < fl_no[0])
 		record_instr(instrs[3], s_a, s_b, instr_lst);
 	else
 		record_instr(instrs[6], s_a, s_b, instr_lst);
+	free(fl_no);
 }
 
 static void	initial_instr(t_list **s_a, t_list **s_b,
