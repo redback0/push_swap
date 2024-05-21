6c81de9cf2361c19ddeb708738f2e921bf03e8f9
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-05-21 16:23:20 +0930

    Finished!!!

--- initial_sort.c
+++ initial_sort.c
@@ -6,7 +6,7 @@
 /*   By: njackson <njackson@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:04:21 by njackson          #+#    #+#             */
-/*   Updated: 2024/05/21 14:16:21 by njackson         ###   ########.fr       */
+/*   Updated: 2024/05/21 16:20:59 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,15 @@ static int	get_init_range(int n, int a)
 	return ((out / 8) + a);
 }
 
+void	init_marker(t_list *s_a, int *marker)
+{
+	marker[3] = ft_lstsize(s_a);
+	marker[0] = marker[3] / 2;
+	marker[1] = marker[3] / 2;
+	marker[2] = marker[3] / 2;
+	marker[5] = marker[3] / 60 + 1;
+}
+
 //#include <stdio.h>
 //#include "stack_printer.c"
 
@@ -45,28 +54,21 @@ void	initial_sort(t_list **s_a, t_list **s_b,
 	t_list **instr_lst, char **instrs)
 {
 	t_stack	*stack;
-	int		range;
-	int		add_range;
-	int		size;
-	int		marker[3];
+	int		marker[6];
 
-	size = ft_lstsize(*s_a);
-	marker[0] = size / 2;
-	marker[1] = size / 2;
-	marker[2] = size / 2;
-	add_range = size / 60;
-	while (size > 3)
+	init_marker(*s_a, marker);
+	while (marker[3] > 3)
 	{
 		stack = ft_lstlast(*s_a)->content;
-		range = get_init_range(get_max(*s_a), add_range);
+		marker[4] = get_init_range(get_max(*s_a), marker[5]);
 		if (stack->fi > 2
-			&& stack->fi >= marker[2] && stack->fi < marker[0] + range)
+			&& stack->fi >= marker[2] && stack->fi <= marker[0] + marker[4])
 		{
 			record_instr(instrs[9], s_a, s_b, instr_lst);
 			marker[0]++;
 		}
 		else if (stack->fi > 2
-			&& stack->fi < marker[2] && stack->fi >= marker[1] - range)
+			&& stack->fi <= marker[2] && stack->fi >= marker[1] - marker[4])
 		{
 			record_instr(instrs[9], s_a, s_b, instr_lst);
 			record_instr(instrs[4], s_a, s_b, instr_lst);
@@ -74,13 +76,14 @@ void	initial_sort(t_list **s_a, t_list **s_b,
 		}
 		else
 			record_instr(instrs[3], s_a, s_b, instr_lst);
-		size = ft_lstsize(*s_a);
+		marker[3] = ft_lstsize(*s_a);
 	}
 }
 //		/*DEBUG*/ft_printf_fd(2, "PICKED INSTRUCTION %s\n", instrs[instr_i]);
 
 //		/*DEBUG*/print_stacks(*s_a, *s_b);
-//		/*DEBUG*/ft_printf_fd(2, "LAST INSTR: %s\n", ft_lstlast(*instr_lst)->content);
-//		/*DEBUG*/ft_printf_fd(2, "RANGE: %d\n", range);
+//		/*DEBUG*/ft_printf_fd(2, "LAST INSTR: %s\n",
+//			ft_lstlast(*instr_lst)->content);
+//		/*DEBUG*/ft_printf_fd(2, "RANGE: %d\n", marker[4]);
 //		/*DEBUG*/ft_printf_fd(2, "MARKERS: %d, %d\n", marker[0], marker[1]);
 //		/*DEBUG*/ft_printf_fd(2, "<<BREAK POINT>>"); getchar();
